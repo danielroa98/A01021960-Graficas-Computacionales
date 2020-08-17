455192a562233a790a665c3966cd4ea249c45c6d
danielroa98 <daniel.roa98@gmail.com>
2020-08-16 19:54:57 -0500

    Ya hay input del teclado, aun no se mueven

--- Actividades/Pong/pong.js
+++ Actividades/Pong/pong.js
@@ -1,35 +1,30 @@
 //Clase para las cosas que se van a manejar en el PONG
-class barra{
-    constructor(x, y, width, height, speed=1){
+class barra {
 
+    constructor(x, y, width, height, speed = 1) {
         this.x = x;
-        this.y =y;
+        this.y = y;
         this.width = width;
         this.height = height;
         this.speed = speed;
-
     }
 
     moveUp() {
-        this.y -= this.speed;        
+        this.y -= this.speed;
     }
-
-    moveDown(){
+    moveDown() {
         this.y += this.speed;
     }
-
-    draw(context){
+    draw(context) {
         context.fillStyle = 'white';
         context.fillRect(this.x, this.y, this.width, this.height);
     }
+    update() {
 
-    update(up, down, left, right) {
-        
     }
-
 }
 
-class pelota{
+class pelota {
     constructor(x, y, radio, speed = 1) {
 
         this.x = x;
@@ -44,41 +39,41 @@ class pelota{
 
     draw(context) {
         context.fillStyle = 'white';
-        context.beginPath();    //Le indicas que empiece a dibujar y cuando detecta un fill para de dibujar
-        context.arc(this.x, this.y, this.radio, 0, Math.PI*2);
+        context.beginPath(); //Le indicas que empiece a dibujar y cuando detecta un fill para de dibujar
+        context.arc(this.x, this.y, this.radio, 0, Math.PI * 2);
         // context.closePath();
         context.fill();
     }
 
-    update(up, down, left, right){
-        if(this.up)
+    update(up, down, left, right) {
+        if (this.up)
             this.y -= this.speed;
         else
-        this.y += this.speed;
-    
+            this.y += this.speed;
+
         if (this.right)
             this.x += this.speed;
         else
             this.x -= this.speed;
-        
+
         if ((this.y - this.radio) <= up)
             this.up = false;
 
         if ((this.y + this.radio) >= down)
             this.up = true;
 
-        if((this.x + this.radio) >= right)
+        if ((this.x + this.radio) >= right)
             this.right = false;
 
-        if((this.x - this.radio) <= left)
+        if ((this.x - this.radio) <= left)
             this.right = true;
     }
 }
 
-function update(canvas, context, barras, bola){
+function update(canvas, context, barras, bola) {
     requestAnimationFrame(() => update(canvas, context, barras, bola));
 
-    context.clearRect(0,0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
     barras.forEach(bola => {
         bola.draw(context);
@@ -89,30 +84,54 @@ function update(canvas, context, barras, bola){
 
 }
 
-function main(){
+function main() {
 
     const canvas = document.getElementById("pongCanvas");
-    const context = canvas.getContext("2d"); 
+    document.addEventListener("keypress", keyPress);
+
+    const context = canvas.getContext("2d");
 
     let barraIzq = new barra(10, 120, 20, 60);
     let barraDer = new barra(570, 120, 20, 60);
-    let bola = new pelota(canvas.width/2, canvas.height/2, 10);
+    let bola = new pelota(canvas.width / 2, canvas.height / 2, 10);
 
     let barras = [];
 
     barras.push(barraIzq, barraDer, bola);
 
-/*     
-    Forma básica de una función anónima
-    gameObjects.forEach(function(object){
-        object.draw(context);
-    }); 
- */
+    /*     
+        Forma básica de una función anónima
+        gameObjects.forEach(function(object){
+            object.draw(context);
+        }); 
+     */
 
     console.log(barras);
 
     //Operador de Morsa
-    barras.forEach(object => object.draw(context));
+    //barras.forEach(object => object.draw(context));
+
+    function keyPress(event) {
+
+        console.log(event.keyCode);
+
+        barras.forEach(object => {
+
+            //Para que detecte si se presiona la W (sin importar si es mayúscula)
+            if (event.keyCode == 87 || event.keyCode == 119) {
+                // object.moveUp();
+                barraDer.speed -= 5;
+            }
+
+            //Para que detecte si se presiona S (sin importar si es mayúscula)
+            else if (event.keyCode == 115 || event.keyCode == 83) {
+                // object.moveDown();
+                barraDer.speed += 5;
+            }
+        });
+    }
 
     update(canvas, context, barras, bola);
+
+
 }
